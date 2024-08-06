bb7e74daf58009edc0ceb365b72cf3dbf6f820a8
Simon Raw <Simonrat@gmail.com>
2024-08-06 15:07:16 +0200

    added inputs for sets and weights

--- trackerinputs.py
+++ trackerinputs.py
@@ -1,3 +1,8 @@
 print ('Hello World')
-SquatSets = int(input('Number of sets:'))
-print (SquatSets)
+squatSets = int(input('Number of sets:'))
+squatWeights = [0]*squatSets
+for i in range(1, squatSets+1):
+    print ('Weight for set', i,':')
+    squatWeights[i-1] = int(input())
+for i in range(0,squatSets):
+    print (squatWeights[i])
