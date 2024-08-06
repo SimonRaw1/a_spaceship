16de1f42eaf418a958e6e61f0225aeb61eb887dc
Simon Raw <Simonrat@gmail.com>
2024-08-06 15:18:10 +0200

    added output string

--- trackerinputs.py
+++ trackerinputs.py
@@ -1,8 +1,14 @@
 print ('Hello World')
 squatSets = int(input('Number of sets:'))
+squatReps = [0]*squatSets
 squatWeights = [0]*squatSets
-for i in range(1, squatSets+1):
-    print ('Weight for set', i,':')
-    squatWeights[i-1] = int(input())
+for i in range(0, squatSets):
+    print ('Reps for set', i+1,':')
+    squatReps[i] = int(input())
+    print ('Weight for set', i+1,':')
+    squatWeights[i] = int(input())
+
+print()
+print ('Squat Session:',)
 for i in range(0,squatSets):
-    print (squatWeights[i])
+   print (squatReps[i],'at',squatWeights[i],'kg')
