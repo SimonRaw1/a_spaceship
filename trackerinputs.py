574667c98ebc0fc069d70b1dfe0782319d409ca7
Simon Raw <Simonrat@gmail.com>
2024-08-17 17:44:34 +0200

    moved sbd input to a function

--- trackerinputs.py
+++ trackerinputs.py
@@ -2,7 +2,7 @@ from datetime import date
 import sys
 
 dateToday = str(date.today())
-
+lift_string = ""
 lift_translation = {
     "S" : "Squat",
     "B" : "Bench",
@@ -16,11 +16,20 @@ def safeinput(inp,type):
         except:
             print("Please enter a valid input")
 
-"""
-Ask the user if they would like to continue and add another session or end
-checks for correct input of Y or N (accepts lowercase)
-asks for a correct input if the user inputs anything else
-"""         
+def choose_lift():
+    """
+    Ask for an input to select the lift
+    runs until a correct input is given
+    """
+    while True:
+        lift = safeinput('Enter the chosen lift (' + ",".join(list(lift_translation.keys())) + "): ", str)     
+        lift_string:str 
+        try:               
+            lift_string = lift_translation[lift.upper()]
+            return lift_string
+        except:
+            pass
+
 def cont(): 
     end = safeinput('Would you like to enter another session? (Y/N): ',str)      
     while True:              
@@ -33,22 +42,22 @@ def cont():
             case _:
                 end = safeinput("Please enter Y or N: ",str)
 
+def checkfile():
+    """
+    Check to see if the textfile exists
+    if it doesnt exist then create the file and print a heading at the top.
+    """
+    try:
+        open(lift_string + ' Sessions.txt')  
+    except:
+        with open (lift_string + ' Sessions.txt', 'w') as text_file:
+            print(f"Record of all Squat Sessions", file=text_file) 
+
 
 #Repeat until user selects not to continue
 while True:       
-        """
-        Asks for an input to select the lift to add to
-        runs until a correct input is given
-        """
-        while True:
-            lift = safeinput('Enter the chosen lift (' + ",".join(list(lift_translation.keys())) + "): ", str)
-            lift_string : str
-            try:               
-                lift_string = lift_translation[lift.upper()]
-                break
-            except:
-                pass
 
+        lift_string = choose_lift()       
         sets_input = safeinput('Number of sets:\n',int)
         print()
 
@@ -57,15 +66,7 @@ while True:
         Inputing 0 will ask them if they want to start again or end
         """
         if sets_input > 0:         
-            """
-            Check to see if the textfile exists
-            if it doesnt exist then create the file and print a heading at the top.
-            """
-            try:
-                open(lift_string + ' Sessions.txt')  
-            except:
-                with open (lift_string + ' Sessions.txt', 'w') as text_file:
-                    print(f"Record of all Squat Sessions", file=text_file) 
+            checkfile()
             """
             Ask for rep and weight inputs
             Add the inputs into the correct textfile
@@ -80,7 +81,8 @@ while True:
             with open(lift_string + ' Sessions.txt', "a") as text_file:
                 print(f"\n{lift_string} Session ({dateToday}):", file=text_file)
                 for i in range(0,sets_input):
-                    print(f"{weight[i]}kg x {reps[i]}", file=text_file)                       
+                    print(f"{weight[i]}kg x {reps[i]}", file=text_file) 
+
             cont()
 
         else:
